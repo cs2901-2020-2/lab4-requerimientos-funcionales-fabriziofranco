93bab2dfb8958a6cb90c881253ce4588697ce5c6
Fabrizio <fabrizio.franco@utec.edu.pe>
2020-09-16 10:03:31 -0500

    En progreso

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -16,15 +16,35 @@ public class  DNASequencer {
         String currentSubsequence;
         for(int iterator=1;iterator<part.size();iterator++){
             currentSubsequence=part.get(iterator);
-            int positionOfPrev=previousSubsequence.indexOf(currentSubsequence);
-            if(positionOfPrev!=-1)
+            String subSequenceinCommon = "";
+            for (int i = 0; i < previousSubsequence.length(); i++) {
+                if (currentSubsequence.contains(previousSubsequence.substring(i))) {
+                    subSequenceinCommon = previousSubsequence.substring(i);
+                    break;
+                }
+            }
+            System.out.print(subSequenceinCommon +" \n\n");
+            int positionOfPrev=previousSubsequence.indexOf(subSequenceinCommon)-1;
+            if(positionOfPrev>0)
                 finalSequence.append(previousSubsequence.substring(0, positionOfPrev -1 ));
             else
                 finalSequence.append(previousSubsequence);
+            System.out.print(finalSequence +" \n\n");
             finalSequence.append(currentSubsequence);
             previousSubsequence=currentSubsequence;
+            System.out.print(finalSequence +" \n\n");
+//
+//
+//            int positionOfPrev=previousSubsequence.indexOf(currentSubsequence);
+//            if(positionOfPrev!=-1)
+//                finalSequence.append(previousSubsequence.substring(0, positionOfPrev -1 ));
+//            else
+//                finalSequence.append(previousSubsequence);
+//            finalSequence.append(currentSubsequence);
+//            previousSubsequence=currentSubsequence;
 
         }
+        System.out.print(finalSequence);
         return finalSequence.toString();
     }
 }
